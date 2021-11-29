e987754d76e1e86fddba5d68468ff7094ff5b25e
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-11-29 20:21:51 +0200

    login system

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -29,7 +29,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password',
+//        'password',
         'remember_token',
     ];
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,17 +3,18 @@
     <div class="row justify-content-md-center">
         <div class="col-sm-5 ">
 <h1>Login page</h1>
-<form>
+<form action="login" method="post">
     <div class="form-group">
+        @csrf
         <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp">
         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1">
+        <input type="password" name="password" class="form-control" id="exampleInputPassword1">
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Login</button>
 </form>
     </div>
     </div>
