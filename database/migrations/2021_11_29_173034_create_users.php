1e655fecdaa9241033c2679c2ac797047dfe82fd
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-11-29 19:44:52 +0200

    DB, migrations, seeding

--- database/migrations/2021_11_29_173034_create_users.php
+++ database/migrations/2021_11_29_173034_create_users.php
@@ -4,23 +4,30 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateUsers extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
+    //composer remove laravel/sanctum
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->string("password");
             $table->timestamps();
+
+//            $table->id();
+//            $table->string('name');
+//            $table->string('email')->unique();
+//            $table->timestamp('email_verified_at')->nullable();
+//            $table->string('password');
+//            $table->rememberToken();
+//            $table->timestamps();
         });
     }
 
